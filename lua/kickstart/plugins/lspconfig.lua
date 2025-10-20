8c9cde0ebc58638db69eecea1f3a6da42fbba8b7
Nicholas Allen <nick@bitwig.com>
2025-10-20 20:52:56 +0200

    gd goes to lsp definition

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -72,6 +72,8 @@ return {
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
+          map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
           map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
