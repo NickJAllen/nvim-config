7295bed5f44a4fd6733d0550949b1fa065037323
Nicholas Allen <nick@bitwig.com>
2025-10-24 07:36:33 +0200

    Telescope color theme picker does live preview

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Problems to solve:
 * Debug last launched quickly
 * Setting exception breakpoints in Java
 * Write a mark plugin so don't need to come up with A-Z name myself and can cycle through the marks
+* Create new classes, records, interfaces easily
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -27,7 +27,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.which-key',
 
-  require 'kickstart.plugins.telescope',
+  -- require 'kickstart.plugins.telescope',
 
   -- require 'kickstart.plugins.lspconfig',
 
@@ -60,6 +60,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  { import = 'custom.themes' },
   { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
