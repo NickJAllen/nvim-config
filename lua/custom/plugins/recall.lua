4249d14db4f759f9e548c856c577a4da5f2fd75b
Nicholas Allen <nick@bitwig.com>
2025-10-25 14:35:50 +0200

    Don't want leader key to invoke the recall marks

--- lua/custom/plugins/recall.lua
+++ lua/custom/plugins/recall.lua
@@ -21,11 +21,11 @@ return {
         wshada = vim.fn.has 'nvim-0.10' == 0,
       }
 
-      vim.keymap.set('n', '<leader>mm', recall.toggle, { noremap = true, silent = true })
-      vim.keymap.set('n', '<leader>mn', recall.goto_next, { noremap = true, silent = true })
-      vim.keymap.set('n', '<leader>mp', recall.goto_prev, { noremap = true, silent = true })
-      vim.keymap.set('n', '<leader>mc', recall.clear, { noremap = true, silent = true })
-      vim.keymap.set('n', '<leader>ml', ':Telescope recall<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', 'mm', recall.toggle, { noremap = true, silent = true })
+      vim.keymap.set('n', 'mn', recall.goto_next, { noremap = true, silent = true })
+      vim.keymap.set('n', 'mp', recall.goto_prev, { noremap = true, silent = true })
+      vim.keymap.set('n', 'mc', recall.clear, { noremap = true, silent = true })
+      vim.keymap.set('n', 'ml', ':Telescope recall<CR>', { noremap = true, silent = true })
     end,
   },
 }
