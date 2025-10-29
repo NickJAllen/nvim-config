789b557ddce4e35c7f19c5be6e357513798371f7
Nicholas Allen <nick@bitwig.com>
2025-10-29 18:07:12 +0100

    Disable gitsigns for now

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -13,9 +13,11 @@
 -- options to `gitsigns.nvim`.
 --
 -- See `:help gitsigns` to understand what the configuration keys do
+
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    enabled = false,
     opts = {
       signs = {
         add = { text = '+' },
